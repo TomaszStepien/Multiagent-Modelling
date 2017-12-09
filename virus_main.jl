2ff36c68aa913502996ca67861285b835ddece86
TomaszStepien <tomaszstepien03@gmail.com>
2017-12-09 17:18:12 +0100

    dehardcoded some parameters

--- virus_main.jl
+++ virus_main.jl
@@ -4,5 +4,22 @@ using PyPlot
 
 include("virus_def.jl")
 
-# simulations 
-go()
+# simulations
+dim = 20
+max_iter = 75
+n_agents = 400
+pct_sick = 0.1
+vaccine_power = 0.3
+mutation_chance = 0.05
+vaccine_desire = 0.1
+default_duration = 30
+
+go(dim = dim,
+   max_iter = max_iter,
+   n_agents = n_agents,
+   pct_sick = pct_sick,
+   vaccine_power = vaccine_power,
+   mutation_chance = mutation_chance,
+   vaccine_desire = vaccine_desire,
+   default_duration = default_duration)
+
